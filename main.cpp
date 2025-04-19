b9a26cbfa42e0d42dc75644c4c1aff2ec2095d28
HODUCVU <hoducvu1234@gmail.com>
2025-04-19 11:29:10 +0700

    Release

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include "TCPService/tcpservice.h"
-// #include "testing.h"
-#include "OverloadSolution/stresstestsystem.h"
-
 #include <QCoreApplication>
+
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+
     TcpService service;
     service.start();
-    // StressTestSystem stresstest;
-    // stresstest.setup(2, 80, 8, 30);
-    // stresstest.start();
+
     return a.exec();
 }
--- testing.h
+++ testing.h
@@ -1,27 +1,27 @@
-#ifndef TESTING_H
-#define TESTING_H
+// #ifndef TESTING_H
+// #define TESTING_H
 
-#include "DeviceStats/SystemStats/systemstats.h"
-#include "DeviceStats/ProcessesStats/processesstats.h"
-#include <QObject>
-#include <QHash>
+// #include "DeviceStats/SystemStats/systemstats.h"
+// #include "DeviceStats/ProcessesStats/processesstats.h"
+// #include <QObject>
+// #include <QHash>
 
-enum class LoadLevel {
-    STATE_NORMAL = 0,
-    STATE_WARNING,
-    STATE_OVERLOADED
-};
+// enum class LoadLevel {
+//     STATE_NORMAL = 0,
+//     STATE_WARNING,
+//     STATE_OVERLOADED
+// };
 
-class Testing : public QObject
-{
-    Q_OBJECT
-public:
-    explicit Testing(QObject *parent = nullptr);
-    int overloadingDetectAdvance(SystemStats &systemStats);
-    void processesFilter(ProcessesStats &processesStats);
-private:
-    QHash<QString, int> readPriority();
-    int maxKillCount = 5; // hoặc giá trị mặc định bạn muốn
-};
+// class Testing : public QObject
+// {
+//     Q_OBJECT
+// private:
+//     QHash<QString, int> readPriority();
+//     int maxKillCount = 5;
+// public:
+//     explicit Testing(QObject *parent = nullptr);
+//     int overloadingDetectAdvance(SystemStats &systemStats);
+//     void processesFilter(ProcessesStats &processesStats);
+// };
 
-#endif // TESTING_H
+// #endif // TESTING_H
